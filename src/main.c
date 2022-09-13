6a0c8cb7be6a9342ff57dce0ce7fd67a46d2b142
ssr <shuran_sun@163.com>
2022-09-13 13:27:35 +0800

    lab1 finish

--- src/main.c
+++ src/main.c
@@ -1,8 +1,46 @@
 #include <aarch64/intrinsic.h>
+#include <kernel/init.h>
+#include <driver/uart.h>
+#include <common/string.h>
+#include <common/defines.h>
 
-static char hello[16];
+#define HELLO_SIZE 16
+
+static char hello[HELLO_SIZE];
+extern char edata[],end[];
+
+void init_bss(){
+    for (u64* p = (u64*)&edata; p < (u64*)&end; p++){
+        memset(p,0,sizeof(u64));
+    }
+}
+
+void define_early_init() {
+
+    strncpy (hello,"hello world!",HELLO_SIZE);
+    
+}
+
+void define_init(){
+    for (int i = 0; i <HELLO_SIZE; i++)
+    {
+        uart_put_char (hello[i]);
+    }
+    
+}
+
 
 NO_RETURN void main()
 {
+    if (cpuid()==0)
+    {
+        do_early_init();
+        do_init(); 
+        init_bss();
+        define_early_init();
+        define_init();
+    }
+    
     arch_stop_cpu();
+
 }
